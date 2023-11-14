59af44222e9496ae23b04e91d244b98a105c4b60
jmviajero <145684339+jmviajero@users.noreply.github.com>
2023-11-15 00:03:15 +0100

    Add files via upload

--- Practica 4/main.ts
+++ Practica 4/main.ts
@@ -11,16 +11,17 @@ import { actualizar } from "./Actualizar/actualizar.ts";
 
 const env= await load();
 
-const MONGO_URL= env.MONGO_URL || Deno.env.get("MONGO_URL");
+let MONGO_URL= env.MONGO_URL || Deno.env.get("MONGO_URL");
 
-const PORT = env.PORT || Deno.env.get("PORT");
+const PORT = env.PORT || Deno.env.get("PORT") || 2999;
 
+if(!MONGO_URL){
+    console.error("No se proporcionó una URL válida para MongoDB");
+    MONGO_URL="mongodb+srv://jmviajero:12345@cluster0.mfoc843.mongodb.net/TARDIS?retryWrites=true&w=majority"
+}
 
 try {
-    if(!MONGO_URL){
-    console.error("No se proporcionó una URL válida para MongoDB");
-    throw new Error("No se proporcionó una URL válida para MongoDB");
-    }
+    
 
 
     await mongoose.connect(MONGO_URL);
