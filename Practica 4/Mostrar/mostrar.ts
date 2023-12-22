279109d72811ca1beb21182b17500f6776c063a2
jmviajero <145684339+jmviajero@users.noreply.github.com>
2023-12-22 12:21:21 +0100

    Update mostrar.ts

--- Practica 4/Mostrar/mostrar.ts
+++ Practica 4/Mostrar/mostrar.ts
@@ -5,26 +5,21 @@ import { TARDISmodel } from "../db/Squema.ts";
 export const mostrar = async(req:Request, res:Response)=>{
     try {
 
-        // Busca la TARDIS por ID
-        const foundTardis = await TARDISmodel.find()
-        .populate({
-            path: "Dimensiones",
-            populate: {
-                path: "Planetas",
-                populate: {
-                    path: "Personas",
-                },
-            },
-        })
-        .exec();
+        const encontrar= await TARDISmodel.find().populate({ path: "Dimensiones",
+                                                               populate: { path: "Planetas",
+                                                                          populate: { path: "Personas",
+                                                                                    },
+                                                                         },
+                                                           }).exec();
+        
 
-        if (!foundTardis) {
-            return res.status(404).json({ error: 'TARDIS no encontrada.' });
+        if (!encontrar) {
+            return res.status(404).json({ "TARDIS no encontrada" });
         }
 
-        res.status(200).json(foundTardis);
+        res.status(200).json(encontrar);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Error interno del servidor.' });
+        res.status(500).json({ "No" });
     }
 }
