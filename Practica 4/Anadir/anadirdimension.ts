74b63c18ac012f2ba8bd049b7315093354cf7ef8
jmviajero <145684339+jmviajero@users.noreply.github.com>
2023-12-22 12:43:07 +0100

    Update anadirdimension.ts

--- Practica 4/Anadir/anadirdimension.ts
+++ Practica 4/Anadir/anadirdimension.ts
@@ -6,19 +6,19 @@ export const añadirdimension = async(Req:Request, Res:Response)=>{
     try {
         const { Nombre, Planetas } = Req.body;
 
-        // Verifica que los planetas proporcionados existan en la base de datos
+       
         const existe = await PlanetaModel.find({ _id:  { $in: Planetas } } );
         if (!existe) {
             return Res.status(400).send("No existen id de los planetas introducidos ");
         }
 
-        // Crea una nueva dimensión
+     
         const newDimension = new DimensionModel({
             Nombre,
             Planetas,
         });
 
-        // Guarda la nueva dimensión en la base de datos
+     
         const savedDimension = await newDimension.save();
 
         Res.status(201).send(savedDimension);
